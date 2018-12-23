2e249ac3cc3f9e699bae8a9e3be9d93dde9cea93
Breuer <k.breuer@dkfz.de>
2018-12-23 14:05:59 +0100

    fix

--- phantompeakqualtools/phantompeakqualtools_png_out.sh
+++
@@ -1,9 +0,0 @@
-
-bam_ip="$1"
-output_basename="$2"
-
-# run phantompeakqualtools
-Rscript /phantompeakqualtools-1.2/run_spp.R -c="$bam_ip" -savp="${output_basename}.pdf" -out="${output_basename}.txt" > "${output_basename}_phantompeakqualtools.log" 2>&1
-
-#convert pdf output to jpg to be incorporated into the html report
-convert -density 500 "${output_basename}.pdf" -quality 100 "${output_basename}.jpg"
